a848edf3fd5f9e9f58750cfb08739aca29b23b94
mvnnn <mavanibhautik@gmail.com>
2016-04-09 15:35:54 +0530

    link with mycourse

--- routes/private/MyCourses.js
+++ routes/private/MyCourses.js
@@ -2,3 +2,23 @@
 exports.myCourses=function(req,res){
   res.render('MyCourses');
 };
+
+exports.aboutCourse=function(req,res){
+  res.render('AboutCourse');
+};
+
+exports.taDetails=function(req,res){
+  res.render('TADetails');
+};
+
+exports.score=function(req,res){
+  res.render('Score');
+};
+
+exports.courseRep=function(req,res){
+  res.render('CourseRep');
+};
+
+exports.courseHome=function(req,res){
+  res.render('CourseHome');
+};
